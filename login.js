af151842ab995b55e8df6aa7f127ed8c2d4f8238
docteurseo <you@example.com>
2022-03-28 11:44:26 +0200

    remove credentalial

--- login.js
+++ login.js
@@ -7,12 +7,7 @@ const mdp = document.querySelector('#mdp')
 const btn = document.querySelector('#submit') 
 
  const firebaseConfig = {
-   apiKey: "AIzaSyBxRQQY2DyBYIdeRoqvWKEVsS1xbLWtqwY",
-   authDomain: "questionapp-6a0dd.firebaseapp.com",
-   projectId: "questionapp-6a0dd",
-   storageBucket: "questionapp-6a0dd.appspot.com",
-   messagingSenderId: "357758807991",
-   appId: "1:357758807991:web:d8d29097f02ed437dec232"
+//   
  };
 
  // Initialize Firebase
